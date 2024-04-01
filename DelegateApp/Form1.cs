e9c82717a29e9cf81411386fb84125d7c98976d0
SuleymanAvci <sa1avci@gmail.com>
2024-04-01 17:23:00 +0300

    first commit

---
+++ DelegateApp/Form1.cs
@@ -0,0 +1,15 @@
+namespace DelegateApp
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DelegateApp/RequestCreators/GetPostRequestCreator.cs
@@ -0,0 +1,27 @@
+﻿using DelegateApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DelegateApp.RequestCreators
+{
+    public class GetPostRequestCreator
+    {
+        public void MakeRequest()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress=new Uri("https://jsonplaceholder.typicode.com");
+
+            var httpRes=client.GetAsync("posts").GetAwaiter().GetResult();
+
+            httpRes.EnsureSuccessStatusCode();
+
+            var resultContent=httpRes.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+            JsonSerializer.Deserialize<List<PostModel>>(resultContent);
+        }
+    }
+}
